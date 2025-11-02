463f7c02ddddf8b758f1b92c60995153cd2e5a53
matias12131 <velazquezmatiasclass@gmail.com>
2025-11-02 00:29:26 -0300

    Se creo el proyecto Maven y se añadio las dependencias al pom.xml por Matias Velazquez

---
+++ proyecto-tp8/src/main/java/ar/edu/unju/escmi/tp8/main/Main.java
@@ -0,0 +1,13 @@
+package main.java.ar.edu.unju.escmi.tp8.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main{
+    public static void main( String[] args )
+    {
+        System.out.println("\n");
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ proyecto-tp8/src/test/java/ar/edu/unju/escmi/tp8/AppTest.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.escmi.tp8;
+
+public class AppTest {
+    
+}
